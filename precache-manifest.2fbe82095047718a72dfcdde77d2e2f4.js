92fa41966228aa3fdd02a1a960038704635f02e6
bletsasAlexandros <bletsasAlexandros@gmail.com>
2020-03-04 19:24:52 +0200

    Updates

---
+++ precache-manifest.2fbe82095047718a72dfcdde77d2e2f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005e2c9497f8ff7e1cdfc873497a1dd2",
+    "url": "/OnliEdu/index.html"
+  },
+  {
+    "revision": "a9eb386e184fd8b2dc1b",
+    "url": "/OnliEdu/static/css/2.1c98e06a.chunk.css"
+  },
+  {
+    "revision": "b95d8c11a3583932e47f",
+    "url": "/OnliEdu/static/css/main.790f5234.chunk.css"
+  },
+  {
+    "revision": "a9eb386e184fd8b2dc1b",
+    "url": "/OnliEdu/static/js/2.8dac4a67.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/OnliEdu/static/js/2.8dac4a67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b95d8c11a3583932e47f",
+    "url": "/OnliEdu/static/js/main.abfe8005.chunk.js"
+  },
+  {
+    "revision": "59aa918c3df8ea866a24",
+    "url": "/OnliEdu/static/js/runtime-main.bc0136a0.js"
+  }
+]);
